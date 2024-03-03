c5c5a22e7e62d07d497bd0a5e6c4e108b36798b4
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-03-03 10:11:45 -0300

    working with new sintax

--- main.py
+++ main.py
@@ -27,5 +27,12 @@ def guess(name):
     current_year = datetime.datetime.now().year
     return render_template("guess.html", year=current_year, person_name=name, age=age, gender=gender)
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_response = requests.get(blog_url)
+    all_posts = blog_response.json()
+    return render_template("blog.html", blog_post=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
