8085c8084fc2396b134973dd544bcf4aa98b7d27
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-03-03 09:46:03 -0300

    playing with APIs and mixing with Flask

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
+import requests
+import json
+import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return "Hello world!"
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year=current_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    age_url = f"https://api.agify.io?name={name}"
+    gender_url = f"https://api.genderize.io?name={name}"
+    
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+
+    current_year = datetime.datetime.now().year
+    return render_template("guess.html", year=current_year, person_name=name, age=age, gender=gender)
 
 if __name__ == "__main__":
     app.run(debug=True)
